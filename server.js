157e9d0847e5d63c8dfd1693fc0a009e1c5f93e5
taehajs <taehajs@naver.com>
2026-07-12 00:56:19 +0900

    asdgasdgasdg

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
-
-// Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
@@ -14,26 +10,25 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-/**
-  * Configure Express middleware
-  */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
 
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-/**
-  * Routes
-  */
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+    res.render('home');
 });
 
 app.get('/organizations', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+    res.render('organizations');
 });
 
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+    res.render('projects');
+});
+
+app.get('/categories', (req, res) => {
+    res.render('categories');
 });
 
 app.listen(PORT, () => {
