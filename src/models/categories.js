6f3542501ee6018ca64d7cb36e4338663f90bb40
taehajs <taehajs@naver.com>
2026-07-15 19:13:42 +0900

    dd

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,9 +1,9 @@
-import { pool } from './db.js';
+import db from './db.js';
 
 export async function getAllCategories() {
     try {
         const sql = 'SELECT * FROM categories ORDER BY name ASC';
-        const result = await pool.query(sql);
+        const result = await db.query(sql);
         return result.rows;
     } catch (error) {
         console.error('Error fetching categories:', error);
