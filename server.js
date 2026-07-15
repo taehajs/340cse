9a7081e2287bf5ec83a0ca9bcefeb4429057ed86
taehajs <taehajs@naver.com>
2026-07-15 18:33:27 +0900

    agsd

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 const PORT = process.env.PORT || 3000;
@@ -20,8 +21,10 @@ app.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 
-app.get('/organizations', (req, res) => {
-    res.render('organizations', { title: 'Organizations' });
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', (req, res) => {
